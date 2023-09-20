6f6bd989d5d15dfc5380395c4644f0fd9634dafb
Svetlana Panenko <svetabagira1986@gmail.com>
2023-09-20 21:14:45 +0300

    upd basename

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { GlobalStyles } from "./UI/globalStyles";
-import { Provider } from "react-redux";
-import store from "./store/store";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { GlobalStyles } from './UI/globalStyles';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyles />
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/car-react/">
         <App />
       </BrowserRouter>
     </Provider>
